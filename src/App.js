b86f36a93993ba1e38e36a194d4d8326a151837a
Negin <negin.khademi@gmail.com>
2024-12-24 13:34:05 +0330

    add,remove,render item and create singleItem component ,...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import './App.css';
 
+import Form from './Form';
+import Items from './Items';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+
 function App() {
-  return <h2>jfhvfu</h2>;
+  const [items, setItems] = useState([]);
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    setItems([...items, newItem]);
+  };
+
+  const removeItem = (itemId) => {
+    const newItems = items.filter((item) => item.id !== itemId);
+    setItems(newItems);
+  };
+
+  return (
+    <section className="section-center">
+      <Form addItem={addItem} />
+      <Items items={items} removeItem={removeItem} />
+    </section>
+  );
 }
 
 export default App;
--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,30 @@
-const Form = () => {
-  return <div>form</div>;
+import { useState } from 'react';
+
+const Form = ({ addItem }) => {
+  const [newItemName, setNewItemName] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItemName) return;
+    addItem(newItemName);
+    setNewItemName('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>gerocery bud</h4>
+      <div className="form-control">
+        <input
+          className="form-input"
+          type="text"
+          value={newItemName}
+          onChange={(event) => setNewItemName(event.target.value)}
+        />
+        <button type="submit" className="btn">
+          add item
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default Form;
